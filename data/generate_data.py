dc90c3ddd58f8b4409e9a55bdd9c328916074a2a
Patricija Brečko <patricija.brecko@marand.si>
2017-10-15 06:56:58 +0200

    neural network

--- data/generate_data.py
+++ data/generate_data.py
@@ -2,6 +2,36 @@ import csv
 import random
 import datetime
 
+def generate_q(i, meal, sweet, wb, cb):
+    if i in categories["meal"]:
+        r = random.uniform(0, 1)
+        q = random.randint(meal[0], meal[1])
+        if (r < 0.1):
+            q = q - meal[2]
+        elif (r > 0.9):
+            q = q + meal[2]
+    elif i in categories["sweet"]:
+        r = random.uniform(0, 1)
+        q = random.randint(sweet[0], sweet[1])
+        if (r < 0.1):
+            q = q - sweet[2]
+        elif (r > 0.9):
+            q = q + sweet[2]
+    elif i in categories["warm beverage"]:
+        r = random.uniform(0, 1)
+        q = random.randint(wb[0], wb[1])
+        if (r < 0.1):
+            q = q - wb[2]
+        elif (r > 0.9):
+            q = q + wb[2]
+    else:
+        r = random.uniform(0, 1)
+        q = random.randint(cb[0], cb[1])
+        if (r < 0.1):
+            q = q - cb[2]
+        elif (r > 0.9):
+            q = q + cb[2]
+    return q
 
 items = ["croissant", "muffin", "capuccino", "sandwich", "tea", "juice", "toast", "water", "cafe latte", "espresso"]
 
@@ -13,148 +43,59 @@ categories = {"meal": [3, 6],
 
 rows = []
 
-for i in range(30):  # primeri za 30 dni
+date = datetime.datetime(2017, 7, 23, 1, 1)  # datum v juliju
 
-    date = datetime.datetime(2017, 9, i+1, 1, 1)  # datum v septembru
+for i in range(90):  # primeri za 90 dni
 
-    isweekday = (date.weekday() < 5)
-
-    if (isweekday):
+    if (date.weekday() < 5): # med tednom
 
         for h in range(7, 22): # gremo cez vse ure, ko delajo
 
-            ll = [12, 13, 14, 20, 21]  # list of hours of LOW traffic
+            # generate_q(i, meal, sweet, wb, cb)
+            ll = [14, 15, 16, 20, 21]  # list of hours of LOW traffic
             if h in ll:
-                # stevilo razlicnih hran na uro
-                r_q = 6
-                q = {}  # seznam hran
-                for a in range(r_q):
-                    # enaka verjetnost za katerikoli izdelek
-                    item = items[random.randint(0, len(items) - 1)]
-                    if item not in q:
-                        q[item] = random.randint(1, 8)  # kolicina je also random
-                # dodamo zdej te hrane v rows
-                for qi in q:
-                    # hour, day, month, year, index izdelka, kolicina izdelka
-                    rows.append([h, date.weekday(), date.month, 2017, items.index(qi), q[qi]])
+                for i in range(len(items)):  # gremo cez vse hrane
+                    q = generate_q(i, [4,6,1], [5,7,1], [12,15,1], [8,10,1])
+                    rows.append([h, date.weekday(), date.month, date.year, i, q])
 
-            lm = [10, 11, 18, 19, 20] # list of hours of MIDDLE traffic
+            lm = [7, 8, 13, 17, 19] # list of hours of MIDDLE traffic
             if h in lm:
-                r_q = random.randint(8, 10) # stevilo razlicnih hran na uro
-                q = {}  # seznam hran
-                for a in range(r_q):
-                    # ce je dopoldne je vecja verjetnost za kavo in sweet, ce popoldne za kavo in meal
-                    r_p = random.uniform(0, 1)
-                    if r_p > 0.3: #izberes kavo in sweet / kavo in meal
-                        cofi = items[random.choice(categories["warm beverage"])]
-                        if cofi not in q:
-                            q[cofi] = random.randint(1, 15)
-                        if (h in [10, 11]): # kava in sweet
-                            swit = items[random.choice(categories["sweet"])]
-                            if swit not in q:
-                                q[swit] = random.randint(1, 15)
-                        else:
-                            meal = items[random.choice(categories["meal"])]
-                            if meal not in q:
-                                q[meal] = random.randint(1, 15)
-                    else: # izberes drugo
-                        r_f = items[random.choice(categories["meal"] + categories["sweet"] + categories["cold beverage"])]
-                        if r_f not in q:
-                            q[r_f] = random.randint(1, 10)
+                for i in range(len(items)):  # gremo cez vse hrane
+                    q = generate_q(i, [8,10,1], [6,8,1], [16,20,1], [9,12,1])
+                    rows.append([h, date.weekday(), date.month, date.year, i, q])
 
-                # dodamo zdej te hrane v rows
-                for qi in q:
-                    # hour, day, month, year, index izdelka, kolicina izdelka
-                    rows.append([h, date.weekday(), date.month, 2017, items.index(qi), q[qi]])
-
-            lh = [7, 8, 9, 15, 16, 17] # list of hours of HIGH traffic
+            lh = [9, 10, 11, 12, 18] # list of hours of HIGH traffic
             if h in lh:
-                r_q = random.randint(9, 10)  # stevilo razlicnih hran na uro
-                q = {}  # seznam hran
-                for a in range(r_q):
-                    cofi = items[random.choice(categories["warm beverage"])]
-                    if cofi not in q:
-                        q[cofi] = random.randint(10, 25)
-                    if (h in [7, 8, 9]):  # kava in sweet ali meal
-                        if (random.uniform(0,1) > 0.4):
-                            swit = items[random.choice(categories["sweet"])]
-                            if swit not in q:
-                                q[swit] = random.randint(5, 20)
-                        else:
-                            meal = items[random.choice(categories["meal"])]
-                            if meal not in q:
-                                q[meal] = random.randint(0, 15)
-
-                    else:  # izberes drugo
-                        if (random.uniform(0,1) < 0.4):
-                            swit = items[random.choice(categories["sweet"])]
-                            if swit not in q:
-                                q[swit] = random.randint(0, 15)
-                        else:
-                            meal = items[random.choice(categories["meal"])]
-                            if meal not in q:
-                                q[meal] = random.randint(5, 20)
-
-                # dodamo zdej te hrane v rows
-                for qi in q:
-                    # hour, day, month, year, index izdelka, kolicina izdelka
-                    rows.append([h, date.weekday(), date.month, 2017, items.index(qi), q[qi]])
+                for i in range(len(items)):  # gremo cez vse hrane
+                    q = generate_q(i, [7,11,1], [10,14,1], [20,25,1], [10,12,1])
+                    rows.append([h, date.weekday(), date.month, date.year, i, q])
 
     elif (date.weekday() == 5):
 
         for h in range(8, 21):  # gremo cez vse ure, ko delajo
 
-            r_q = random.randint(6, 9)  # stevilo razlicnih hran na uro
-            q = {}  # seznam hran
-            for a in range(r_q):
-                if (h in [9, 10, 11, 12, 16, 17, 18, 19]):
-                    cofi = items[random.choice(categories["warm beverage"])]
-                    if cofi not in q:
-                        q[cofi] = random.randint(10, 30)
-                    rest = items[random.choice(categories["cold beverage"] + categories["meal"] + categories["sweet"])]
-                    if rest not in q:
-                        q[rest] = random.randint(3, 20)
-                else:
-                    cofi = items[random.choice(categories["warm beverage"])]
-                    if cofi not in q:
-                        q[cofi] = random.randint(5, 20)
-                    rest = items[random.choice(categories["cold beverage"] + categories["meal"] + categories["sweet"])]
-                    if rest not in q:
-                        q[rest] = random.randint(1, 13)
-            # dodamo zdej te hrane v rows
-            for qi in q:
-                # hour, day, month, year, index izdelka, kolicina izdelka
-                rows.append([h, date.weekday(), date.month, 2017, items.index(qi), q[qi]])
+            # generate_q(i, meal, sweet, wb, cb)
+            ll = [9, 10, 11, 12, 13]  # list of hours of LOW traffic
+            if h in ll:
+                for i in range(len(items)):  # gremo cez vse hrane
+                    q = generate_q(i, [9, 12, 1], [8, 11, 1], [17, 20, 1], [15, 18, 1])
+                    rows.append([h, date.weekday(), date.month, date.year, i, q])
+
+            lm = [8, 14, 15, 16, 17, 18, 19, 20]  # list of hours of MIDDLE traffic
+            if h in lm:
+                for i in range(len(items)):  # gremo cez vse hrane
+                    q = generate_q(i, [7, 9, 1], [6, 8, 1], [14, 18, 1], [12, 15, 1])
+                    rows.append([h, date.weekday(), date.month, date.year, i, q])
 
     else:
 
         for h in range(8, 15):  # gremo cez vse ure, ko delajo
 
-            r_q = random.randint(6, 7)  # stevilo razlicnih hran na uro
-            q = {}  # seznam hran
-            for a in range(r_q):
-                if (h in [9, 10, 11]):
-                    cofi = items[random.choice(categories["warm beverage"])]
-                    if cofi not in q:
-                        q[cofi] = random.randint(5, 20)
-                    rest = items[
-                        random.choice(categories["cold beverage"] + categories["meal"] + categories["sweet"])]
-                    if rest not in q:
-                        q[rest] = random.randint(1, 15)
-                else:
-                    cofi = items[random.choice(categories["warm beverage"])]
-                    if cofi not in q:
-                        q[cofi] = random.randint(3, 13)
-                    rest = items[
-                        random.choice(categories["cold beverage"] + categories["meal"] + categories["sweet"])]
-                    if rest not in q:
-                        q[rest] = random.randint(1, 8)
+            for i in range(len(items)):  # gremo cez vse hrane
+                q = generate_q(i, [8, 10, 1], [9, 11, 1], [13, 15, 1], [8, 10, 1])
+                rows.append([h, date.weekday(), date.month, date.year, i, q])
 
-            # dodamo zdej te hrane v rows
-            for qi in q:
-                # hour, day, month, year, index izdelka, kolicina izdelka
-                rows.append([h, date.weekday(), date.month, 2017, items.index(qi), q[qi]])
-
+    date = date + datetime.timedelta(days=1)
 
 for r in rows:
     print(r)
@@ -176,7 +117,11 @@ print(len(rows))
     # categories
 
 
-with open('data_01.csv', 'w', newline='') as csvfile:
+with open('data_03_nn.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
+    counter = 0
     for r in rows:
         writer.writerow(r)
+        if counter > 20:
+            break
+        counter+=1
--- data/linear_regression.py
+++ data/linear_regression.py
@@ -1,80 +1,85 @@
-from collections import defaultdict
-import numpy
-from scipy.optimize import fmin_l_bfgs_b
-import scipy
-import scipy.sparse
-
-
-import scipy.sparse as sp
+from sklearn import linear_model, cross_validation
+import csv
 import numpy as np
+import matplotlib.pyplot as plt
 
-def append_ones(X):
-    if sp.issparse(X):
-        return sp.hstack((np.ones((X.shape[0], 1)), X)).tocsr()
+
+def prepare_data(r):
+    row = []
+    # ure
+    for i in range(8, 22):
+        if (int(r[0]) == i):
+            row.append(1)
+        else:
+            row.append(0)
+    # dan v tednu
+    for i in range(7):
+        if (int(r[1]) == i):
+            row.append(1)
+        else:
+            row.append(0)
+    # vikend
+    if (int(r[1]) > 4):
+        row.append(1)
     else:
-        return np.hstack((np.ones((X.shape[0], 1)), X))
-
-def hl(x, theta):
+        row.append(0)
+    # mesec
     """
-    Napovej verjetnost za razred 1 glede na podan primer (vektor vrednosti
-    znacilk) in vektor napovednih koeficientov theta.
+    for i in range(1, 13):
+        if (r[2] == i):
+            row.append(1)
+        else:
+            row.append(0)
     """
-    return x.dot(theta)
+    # item
+    for i in range(10):
+        if (int(r[4]) == i):
+            row.append(1)
+        else:
+            row.append(0)
 
-def cost_grad_linear(theta, X, y, lambda_):
-    #do not regularize the first element
-    sx = hl(X, theta)
-    j = 0.5*numpy.mean((sx-y)*(sx-y)) + 1/2.*lambda_*theta[1:].dot(theta[1:])/y.shape[0]
-    grad = X.T.dot(sx-y)/y.shape[0] + numpy.hstack([[0.],lambda_*theta[1:]])/y.shape[0]
-    return j, grad
+    return row
 
-class LinearLearner(object):
+data = []
+y = []
 
-    def __init__(self, lambda_=0.0):
-        self.lambda_ = lambda_
+with open('data_03.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile)
+    for r in reader:
+        row = prepare_data(r)
+        data.append(row)
+        y.append(int(r[5]))
 
-    def __call__(self, X, y):
-        """
-        Zgradi napovedni model za ucne podatke X z razredi y.
-        """
-        X = append_ones(X)
 
-        th = fmin_l_bfgs_b(cost_grad_linear,
-            x0=numpy.zeros(X.shape[1]),
-            args=(X, y, self.lambda_))[0]
+lm = linear_model.LinearRegression()
+model = lm.fit(data,y)
 
-        return LinearRegClassifier(th)
+print(lm.predict(prepare_data([9,0,9,2017,0])))
+print(lm.predict(prepare_data([21,3,9,2017,0])))
 
-class LinearRegClassifier(object):
+#print(lm.score(data,y))
 
-    def __init__(self, th):
-        self.th = th
+mse = np.mean((y - lm.predict(data)) ** 2)
+print(mse)
 
-    def __call__(self, x):
-        """
-        Napovej razred za vektor vrednosti znacilk. Vrni
-        seznam [ verjetnost_razreda_0, verjetnost_razreda_1 ].
-        """
-        x = numpy.hstack(([1.], x))
-        return hl(x, self.th)
+"""
+X_train, X_test, Y_train, Y_test = cross_validation.train_test_split(data, y, test_size=0.20)
 
-if __name__ == "__main__":
-    #testiranje
+lm = linear_model.LinearRegression()
+lm.fit(X_train, Y_train)
+pred_train = lm.predict(X_train)
+pred_test = lm.predict(X_test)
 
-    verbose = 0
 
-    import Orange
+mse = np.mean((Y_train - lm.predict(X_train)) ** 2)
+print("trein", mse)
 
-    def iris():
-        data = Orange.data.Table("iris")
-        return data.X, data.Y
+mse = np.mean((Y_test - lm.predict(X_test)) ** 2)
+print("test", mse)
 
-    X,y = iris()
-    print(y)
-    Xsp = scipy.sparse.csr_matrix(X)
 
-    lr = LinearLearner(lambda_=1.)
-    linear = lr(Xsp,y)
-
-    #for a in X:
-        #print(linear(a))
+plt.scatter(lm.predict(X_train), lm.predict(X_train) - Y_train, c='b', s=40, alpha=0.5)
+plt.scatter(lm.predict(X_test), lm.predict(X_test) - Y_test, c='g', s=40)
+plt.hlines(y=0, xmin=0, xmax=50)
+plt.show(block=True)
+"""
